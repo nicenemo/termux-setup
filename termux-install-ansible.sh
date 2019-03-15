a0153e141db6f97265a6b26b5e8665874cbe2621
Hans Kruse <kruse@hanskruse.eu>
2019-03-15 23:15:42 +0100

    Update termux-install-ansible.sh

--- termux-install-ansible.sh
+++ termux-install-ansible.sh
@@ -17,7 +17,7 @@ pip list --outdated --format=freeze | \
   grep -v '^\-e' | \
   cut -d = -f 1 | \
   xargs -n1 pip install -U && \
-#  The pynacl dependency originally did not install because
-#  it gave problems  building dependencies'
+# The pynacl dependency originally did not install because
+# it gave problems building dependencies'
 pip install --upgrade pynacl 
 pip install --upgrade ansible
